5f7e150af0f264ebfda41179be80bd6ab4b2e8ca
jeremiahsalamat <jeremiahsalamat@hotmail.com>
2026-04-02 18:00:21 -0700

    Update and rename SQLQuery_Requirement3.sql to SQLQuery_Req3.sql

--- data/sql/SQLQuery_Req3.sql
+++ data/sql/SQLQuery_Req3.sql
@@ -1,4 +1,4 @@
--- Requirement 2
+-- Requirement 3
 
 SELECT COUNT(bp.member_id) AS [Patient Count], bp.gender, AVG(bp.systolic_reading) AS [Average Systolic], AVG(bp.diastolic_reading) AS [Average Diastolic]
 FROM dbo.hedis_blood_pressure bp
