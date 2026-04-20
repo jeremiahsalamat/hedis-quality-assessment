08cec92bd2aec34fd96b4ef2dd289db2d6e6c92c
jeremiahsalamat <jeremiahsalamat@hotmail.com>
2026-04-20 11:17:57 -0700

    Update SQL scripts to reference renamed data files

--- data/sql/SQLQuery_Req3.sql
+++ data/sql/SQLQuery_Req3.sql
@@ -1,5 +1,9 @@
 -- Requirement 3
 
-SELECT COUNT(bp.member_id) AS [Patient Count], bp.gender, AVG(bp.systolic_reading) AS [Average Systolic], AVG(bp.diastolic_reading) AS [Average Diastolic]
-FROM dbo.hedis_blood_pressure bp
+SELECT 
+   COUNT(bp.member_id) AS [Patient Count]
+  ,bp.gender
+  ,AVG(bp.systolic_reading) AS [Average Systolic]
+  ,AVG(bp.diastolic_reading) AS [Average Diastolic]
+FROM dbo.hedis_cbp_controlling_blood_pressure bp
 GROUP BY bp.gender;
